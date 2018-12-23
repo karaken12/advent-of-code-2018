531c6a6a8b1a60ec3c1e1f086239dc8571f1ec85
Tom Potts <karaken12@gmail.com>
2018-12-23 20:22:02 +0000

    Answer to part one.

--- 01-frequency/src/frequency/core.clj
+++ 01-frequency/src/frequency/core.clj
@@ -1,12 +1,18 @@
 (ns frequency.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 (defn sum-frequency-changes
   "Add together frequency changes"
   [& changes]
   (reduce + changes))
+
+(defn -main
+  "Read from a file to get frequency changes."
+  [& args]
+  (let [changes (map read-string (str/split-lines (slurp "resources/changes.txt")))]
+    (println changes)
+    (println "Sum of frequency changes is:")
+    (println (apply sum-frequency-changes changes))
+    )
+  )
